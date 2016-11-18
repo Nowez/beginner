7cb99eceac454943478448a308e1d237422da474
Nowez <william.palsbacke@gmail.com>
2016-11-18 22:01:28 +0100

    Unity Add

---
+++ Unity/Backups/Gamez/Assets/BallHealth.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var maxFallDistance = -10;
+
+function Update () {
+
+  if (transform.position.y <= maxFallDistance)
+  {
+    RestartLevel();
+  }
+}
+
+function RestartLevel () {
+  GetComponent.<AudioSource>().Play();
+  yield WaitForSeconds (GetComponent.<AudioSource>().clip.length);
+  Application.LoadLevel("Level01");
+}
