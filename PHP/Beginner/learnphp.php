d2e31eb97f9418ec27dc37a7e2bee4cf5b33d6ce
Nowez <william.palsbacke@gmail.com>
2016-11-19 14:21:38 +0100

    learned som HTML5

---
+++ PHP/Beginner/learnphp.php
@@ -0,0 +1,22 @@
+<html>
+
+  <head>
+    <title>Information gathered</title>
+  </head>
+
+  <body>
+
+    <?php
+
+    echo "<p>Data processed at </p>";
+
+    date_default_timezone_set('UTC');
+
+    echo date('h:i:s:u a, l F jS Y e');
+
+    echo "</p>";
+    
+    ?>
+
+  </body>
+</html>
